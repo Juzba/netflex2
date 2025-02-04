bc7709ad7c39b6aeac0868de267d57a2c9baf7a4
Jiří Strnadel <Juzba88@gmail.com>
2025-02-04 05:06:19 +0100

    Netfflex - film category

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <h1>Nadpis</h1>;
+};
+
+export default Button;
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import "./Card.css"
+
+const Card = () => {
+    return <h1>Nadpis</h1>;
+};
+
+export default Card;
